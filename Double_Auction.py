676a7839522a7fc866eee1ec9545140c1bcab3fe
Luis G Bornia <luis.gustavo.bornia@gmail.com>
2022-02-04 20:52:13 -0300

    double auction python inicio

--- Double_Auction.py
+++ Double_Auction.py
@@ -1,8 +1,18 @@
 from mesa import Agent, Model
 from mesa.time import RandomActivation
+import random
 import pandas as pd
 
 
+global acao = 2/3
+global din = 1-acao
+
 class Agente(Agent):
 
-    __init__(self)
+    def __init__(self, N):
+        self.qtde = random.randint(100, 300)
+        self.cash = random.randint(5000, 10000)
+        self.preco = random.randint(10, 100)
+        self.util = (self.qtde**acao)+(self.cash**din)
+        self.sold = False
+        
